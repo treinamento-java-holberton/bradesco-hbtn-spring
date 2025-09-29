080909efdf8de3c2e236cf8557844598d1886312
Arthur Queiroz <arthurclaizoni@gmail.com>
2025-09-29 13:05:37 -0300

    0. Testando um Serviço com Mockito no Spring - Produto

--- spring-mocking/1x01/src/test/java/com/example/demo/service/ProdutoServiceTest.java
+++ spring-mocking/1x01/src/test/java/com/example/demo/service/ProdutoServiceTest.java
@@ -1,12 +1,21 @@
 package com.example.demo.service;
 
 
+import com.example.demo.model.Produto;
 import com.example.demo.repository.ProdutoRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 
 @ExtendWith(MockitoExtension.class)
@@ -23,12 +32,20 @@ class ProdutoServiceTest {
 
     @Test
     void deveRetornarProdutoQuandoIdExistir() {
-        // implemente
+        var produto = new Produto();
+        when(produtoRepository.findById(any())).thenReturn(Optional.of(produto));
+
+        assertThat(produtoService.buscarPorId(1L)).isEqualTo(produto);
+        verify(produtoRepository).findById(1L);
     }
 
 
     @Test
     void deveLancarExcecaoQuandoProdutoNaoExistir() {
-        // implemente
+
+        assertThatThrownBy(() -> produtoService.buscarPorId(1L))
+                .isInstanceOf(RuntimeException.class)
+                .hasMessage("Produto não encontrado");
+        verify(produtoRepository).findById(1L);
     }
 }
