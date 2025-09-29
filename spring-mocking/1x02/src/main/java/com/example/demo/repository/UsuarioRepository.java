9d7ca078c0cc3fe485b75e89b77607ecf4b77531
Arthur Queiroz <arthurclaizoni@gmail.com>
2025-09-29 13:27:55 -0300

    1. Testando um Serviço com Mockito no Spring - Usuário

--- spring-mocking/1x02/src/main/java/com/example/demo/repository/UsuarioRepository.java
+++ spring-mocking/1x02/src/main/java/com/example/demo/repository/UsuarioRepository.java
@@ -3,9 +3,16 @@ package com.example.demo.repository;
 
 import com.example.demo.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 
 @Repository
-public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+public interface UsuarioRepository {
+
+    Optional<Usuario> findById(Long id);
+
+    Usuario save(@NonNull Usuario usuario);
 }
